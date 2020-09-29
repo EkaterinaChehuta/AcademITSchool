79ee31cc31a5d454c1a707dc16fca86befa3c97c
Ekaterina <katya.chehuta@gmail.com>
2020-09-29 16:12:21 +0700

    Исправления от 15.09.

--- ArrayListHome/src/ekaterina/chehuta/array/list/home/ArrayListHome.java
+++ ArrayListHome/src/ekaterina/chehuta/array/list/home/ArrayListHome.java
@@ -1,126 +1,77 @@
 package ekaterina.chehuta.array.list.home;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
 
-public class ArrayListHome<E> {
-    private String[] list;
-    private int size;
+public class ArrayListHome {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = readEndWrite("C:\\Users\\Ekaterina\\IdeaProjects\\AcademITSchool\\ArrayListHome\\src\\ekaterina\\chehuta\\array\\list\\home\\list");
+        System.out.println(arrayList);
 
-    // конструктор заполнения списка из файла
-    public ArrayListHome(String address) {
-        try (FileReader fileReader = new FileReader(address)) {
-            Scanner scanner = new Scanner(fileReader);
-            list = new String[size];
+        oddNumbersList(arrayList);
+        System.out.println(arrayList);
 
-            String x;
-            int i = 0;
-            while (scanner.hasNext()) {
-                x = scanner.next();
+        ArrayList<String> arrayList1 = getLinkedList(arrayList);
+        System.out.println(arrayList1);
+    }
 
-                if (size >= list.length) {
-                    ensureCapacity(size);
-                }
-
-                list[i] = x;
-                i++;
-                size++;
+    public static ArrayList<String> readEndWrite(String fileReader) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileReader))) {
+            String string;
+            while ((string = bufferedReader.readLine()) != null) {
+                arrayList.add(string);
             }
-
-            trimToSize();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ignore) {
         }
+
+        return arrayList;
     }
 
-    // конструктор копирования списка
-    public ArrayListHome(ArrayListHome<E> arrayListHome) {
-        list = Arrays.copyOf(arrayListHome.list, arrayListHome.size);
-        size = arrayListHome.size;
-    }
+    //    Есть список из целых чисел. Удалить из него все четные числа. В
+//    этой задаче новый список создавать нельзя
+    public static void oddNumbersList(ArrayList<String> arrayList) {
+        int[] integerList = new int[arrayList.size()];
 
-    // конструктор заполнения списка из массива
-    public ArrayListHome(String... arrayList) {
-        list = Arrays.copyOf(arrayList, arrayList.length);
-        size = arrayList.length;
-    }
-
-    public int getSize() {
-        return this.size;
-    }
-
-    // увеличение размера списка в 2 раза
-    public void ensureCapacity(int minCapacity) {
-        list = Arrays.copyOf(list, minCapacity * 2 + 1);
-    }
-
-    public void oddNumbersList() {
-        int[] integerList = new int[size];
-
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < arrayList.size(); i++) {
             try {
-                integerList[i] = Integer.parseInt(list[i]);
+                integerList[i] = Integer.parseInt(arrayList.get(i));
 
                 if (integerList[i] % 2 == 0) {
-                    remove(i);
+                    arrayList.remove(i);
                     i--;
                 }
-            } catch (NumberFormatException remove) {
-                remove(i);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Список не состоит из целых чисел.");
             }
         }
-
-        trimToSize();
     }
 
-    public ArrayListHome<String> getLinkedList() {
-        ArrayListHome<String> newArrayListHome = new ArrayListHome<>(list);
-        newArrayListHome.size = newArrayListHome.list.length;
-        int[] integerNewList = new int[newArrayListHome.size];
+    //    Надо создать новый список, в котором будут элементы первого списка
+//    в таком же порядке, но без повторений
+    public static ArrayList<String> getLinkedList(ArrayList<String> arrayList) {
+        ArrayList<String> newArrayList = new ArrayList<>(arrayList);
+        int[] integerNewList = new int[arrayList.size()];
 
-        for (int i = 0; i < newArrayListHome.size; i++) {
+        for (int i = 0; i < newArrayList.size(); i++) {
             try {
-                integerNewList[i] = Integer.parseInt(newArrayListHome.list[i]);
+                integerNewList[i] = Integer.parseInt(arrayList.get(i));
 
-                for (int j = i + 1; j < newArrayListHome.size - 1; j++) {
+                for (int j = i + 1; j < newArrayList.size() - 1; j++) {
+                    integerNewList[j] = Integer.parseInt(arrayList.get(j));
 
-                    try {
-                        integerNewList[j] = Integer.parseInt(newArrayListHome.list[j]);
-
-                        if (integerNewList[i] == integerNewList[j]) {
-                            newArrayListHome.remove(j);
-                            j--;
-                        }
-                    } catch (NumberFormatException remove) { // если встречается буква
-                        newArrayListHome.remove(j);
+                    if (integerNewList[i] == integerNewList[j]) {
+                        newArrayList.remove(j);
                         j--;
                     }
                 }
-
-            } catch (NumberFormatException remove) { // если встречается буква
-                newArrayListHome.remove(i);
-                i--;
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Список не состоит из целых чисел.");
             }
         }
 
-        trimToSize();
-        return newArrayListHome;
-    }
-
-    public void remove(int index) {
-        if (size - 1 - index >= 0) System.arraycopy(list, index + 1, list, index, size - 1 - index);
-
-        size--;
-    }
-
-    public void trimToSize() {
-        if (size < list.length) {
-            list = Arrays.copyOf(list, size);
-        }
-    }
-
-    public String toString() {
-        return Arrays.toString(list);
+        return newArrayList;
     }
 }
