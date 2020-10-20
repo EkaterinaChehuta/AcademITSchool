764f11bd6ef20746c01e593eae51247488d77cea
Ekaterina <katya.chehuta@gmail.com>
2020-10-20 10:55:36 +0700

    Исправления от 14.10

--- ArrayListHome/src/ekaterina/chehuta/array/list/home/ArrayListHome.java
+++ ArrayListHome/src/ekaterina/chehuta/array/list/home/ArrayListHome.java
@@ -6,39 +6,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ArrayListHome {
-    public static void main(String[] args) {
-        ArrayList<String> stringsList = getNumbersList("C:\\Users\\Ekaterina\\IdeaProjects\\AcademITSchool\\ArrayListHome\\src\\ekaterina\\chehuta\\array\\list\\home\\list");
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> stringsList = getStringsList("ArrayListHome/list.txt");
         System.out.println(stringsList);
 
         ArrayList<Integer> integersList = getIntegersList(stringsList);
 
-        removesEvenIntegersFromList(integersList);
+        removeEvenIntegersFromList(integersList);
         System.out.println(integersList);
 
         ArrayList<Integer> newIntegersList = getUniqueIntegersList(integersList);
         System.out.println(newIntegersList);
     }
 
-    public static ArrayList<String> getNumbersList(String Url) {
+    public static ArrayList<String> getStringsList(String fileName) throws IOException {
         ArrayList<String> arrayList = new ArrayList<>();
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(Url))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String string;
 
             while ((string = bufferedReader.readLine()) != null) {
                 arrayList.add(string);
             }
-        } catch (IOException e) {
-            throw new NumberFormatException("Список не состоит из целых чисел.");
         }
 
         return arrayList;
     }
 
-    public static ArrayList<Integer> getIntegersList(ArrayList<String> arrayList) {
+    public static ArrayList<Integer> getIntegersList(ArrayList<String> stringsList) {
         ArrayList<Integer> integersList = new ArrayList<>();
 
-        for (String s : arrayList) {
+        for (String s : stringsList) {
             try {
                 integersList.add(Integer.parseInt(s));
             } catch (NumberFormatException e) {
@@ -51,7 +49,7 @@ public class ArrayListHome {
 
     // Есть список из целых чисел. Удалить из него все четные числа. В
     // этой задаче новый список создавать нельзя
-    public static void removesEvenIntegersFromList(ArrayList<Integer> integersList) {
+    public static void removeEvenIntegersFromList(ArrayList<Integer> integersList) {
         for (int i = 0; i < integersList.size(); i++) {
             if (integersList.get(i) % 2 == 0) {
                 integersList.remove(i);
@@ -63,17 +61,12 @@ public class ArrayListHome {
     // Надо создать новый список, в котором будут элементы первого списка
     // в таком же порядке, но без повторений
     public static ArrayList<Integer> getUniqueIntegersList(ArrayList<Integer> integersList) {
-        ArrayList<Integer> uniqueIntegersList = new ArrayList<>();
+        ArrayList<Integer> uniqueIntegersList = new ArrayList<>(integersList.size());
 
-        e:
         for (Integer integer : integersList) {
-            for (Integer value : uniqueIntegersList) {
-                if (integer.equals(value)) {
-                    continue e;
-                }
+            if (!uniqueIntegersList.contains(integer)) {
+                uniqueIntegersList.add(integer);
             }
-
-            uniqueIntegersList.add(integer);
         }
 
         return uniqueIntegersList;
